a702621cec5fbdb9a6fb0c7a0305cc5170db23a6
charan teja <charaneie22@gmail.com>
2017-11-08 23:24:58 +0530

    Added Tick symbol

--- src/Components/Price.js
+++ src/Components/Price.js
@@ -27,9 +27,9 @@ AUDprice(){
      <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"> {this.props.currency} 
      <span className="caret"></span></button>
      <ul className="dropdown-menu">
-     <li><a href="#" onClick={this.INRprice.bind(this) }> {this.props.currency==='INR' ? "\u2714" : ''} INR</a></li>
-     <li><a href="#" onClick={this.ETHprice.bind(this) }> {this.props.currency==='ETH' ? "\u2714" : ''} ETH</a></li>
-     <li><a href="#" onClick={this.AUDprice.bind(this) }> {this.props.currency==='AUD' ? "\u2714" : ''} AUD</a></li>
+     <li><a href="#" onClick={this.INRprice.bind(this) }>INR {this.props.currency==='INR' ? "\u2714" : ''}</a></li>
+     <li><a href="#" onClick={this.ETHprice.bind(this) }>ETH {this.props.currency==='ETH' ? "\u2714" : ''}</a></li>
+     <li><a href="#" onClick={this.AUDprice.bind(this) }>AUD {this.props.currency==='AUD' ? "\u2714" : ''}</a></li>
   </ul>
 </div>
         </div>
