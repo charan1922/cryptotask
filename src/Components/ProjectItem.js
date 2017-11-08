26a1eb309899c513ce05e4b938160461193b837f
charan teja <charaneie22@gmail.com>
2017-11-08 19:09:45 +0530

    Add Currency Symbols

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 import './Project.css';
-
+var NF = require('react-number-format')
 
 class ProjectItem extends Component {
 
     render() {
  let x ='';
       if(this.props.Project.price_inr){
-        x = this.props.Project.price_inr;
+       
+        x= <NF value= {this.props.Project.price_inr} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'₹'} />
         }
 
         else
         
         if(this.props.Project.price_aud){
-          x = this.props.Project.price_aud;
+          
+          x = <NF value= {this.props.Project.price_aud} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'$'} />
           }
           else 
           
           if(this.props.Project.price_eth){
-            x = this.props.Project.price_eth;
+            
+            x = <NF value= {this.props.Project.price_eth} displayType={'text'} decimalPrecision={2} thousandSeparator={true} suffix={'  ETH'} />
             }
 
             else 
 
         {
-           x= this.props.Project.price_usd; 
+          
+           x= <NF value= {this.props.Project.price_usd} displayType={'text'} decimalPrecision={2} thousandSeparator={true} prefix={'$'} />
         }
         
 
