50cf3ad52bb9d326a2d2bb39197389129610e7da
charan teja <charaneie22@gmail.com>
2017-11-08 23:13:49 +0530

    Added Tick

--- src/Components/Price.js
+++ src/Components/Price.js
@@ -19,7 +19,7 @@ AUDprice(){
 
 
     render() {
- 
+
 
     return (
      <div  id="projects">
@@ -28,8 +28,8 @@ AUDprice(){
      <span className="caret"></span></button>
      <ul className="dropdown-menu">
      <li><a href="#" onClick={this.INRprice.bind(this) }> {this.props.currency==='INR' ? "\u2714" : ''} INR</a></li>
-     <li><a href="#" onClick={this.ETHprice.bind(this) }>ETH</a></li>
-     <li><a href="#" onClick={this.AUDprice.bind(this) }>AUD</a></li>
+     <li><a href="#" onClick={this.ETHprice.bind(this) }> {this.props.currency==='ETH' ? "\u2714" : ''} ETH</a></li>
+     <li><a href="#" onClick={this.AUDprice.bind(this) }> {this.props.currency==='AUD' ? "\u2714" : ''} AUD</a></li>
   </ul>
 </div>
         </div>
